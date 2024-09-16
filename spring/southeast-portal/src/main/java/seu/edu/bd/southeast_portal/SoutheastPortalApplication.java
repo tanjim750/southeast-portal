5fd8163127c9099c4fe2d5e9572110e52b4b552e
tanjim750 <mdtanjim750@gmail.com>
2024-09-16 09:27:00 +0600

    sotheast portal project started

---
+++ spring/southeast-portal/src/main/java/seu/edu/bd/southeast_portal/SoutheastPortalApplication.java
@@ -0,0 +1,13 @@
+package seu.edu.bd.southeast_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoutheastPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoutheastPortalApplication.class, args);
+	}
+
+}
---
+++ spring/southeast-portal/src/test/java/seu/edu/bd/southeast_portal/SoutheastPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package seu.edu.bd.southeast_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoutheastPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
